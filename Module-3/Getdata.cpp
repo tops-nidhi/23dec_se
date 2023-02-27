31d881ea32b816ced5be588c342cbd32507f5a88
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-02-27 16:45:46 +0530

    add

---
+++ Module-3/Getdata.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int no;
+    cout<<"\nEnter the value of no:";
+    cin>>no;
+    cout<<"\nValue of no is:"<<no;
+}
---
+++ Module-3/Hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+// using namespace std;
+int main()
+{
+    std::cout<<"Hello C++!";
+    std::cout<<std::endl<<"This is my first program.";
+}
